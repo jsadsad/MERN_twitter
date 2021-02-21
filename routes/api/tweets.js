9e2875451a50c3c2f0099e50f3df2b4e8d1974e4
JCS <joshsad2@gmail.com>
2021-02-21 15:35:51 -0800

    able to fetch tweet id and tweet user id

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -7,10 +7,22 @@ const Tweet = require('../../models/Tweet')
 const validateTweetInput = require('../../validation/tweets')
 
 router.get('/', (req, res) => {
-  Tweet.find()
-    .sort({ date: -1 })
+  Tweet.find() //we don't add passport authenticate because it doesn't matter which user we're logging in with for this particular route
+    .sort({ date: -1 }) // newest first
     .then((tweets) => res.json(tweets))
-    .catch((err) => res.status(404).json({ notweetsfound: 'No tweets found' }))
+    .catch((err) => res.status(400).json({ notweetsfound: 'No tweets found' }))
+})
+
+router.get('/user/:user_id', (req, res) => {
+  Tweet.find({ user: req.params.user_id })
+    .then((tweets) => res.json(tweets))
+    .catch((err) => res.status(400).json(err))
+})
+
+router.get('/:id', (req, res) => {
+  Tweet.findById(req.params.id)
+    .then((tweet) => res.json(tweet))
+    .catch((err) => res.status(400).json(err))
 })
 
 router.post(
