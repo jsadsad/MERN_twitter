1ab98f451bfab02413e73fdec546c95b15c65f29
JCS <joshsad2@gmail.com>
2021-02-20 23:19:31 -0800

    added validations for register

---
+++ validation/register.js
@@ -0,0 +1,46 @@
+//This makes sure sign up information makes sense
+
+const Validator = require('validator')
+const validText = require('./valid-text')
+
+module.exports = function validateRegisterInput() {
+  let errors = {}
+
+  data.handle = validText(data.handle) ? data.handle : ''
+  data.email = validText(data.email) ? data.email : ''
+  data.password = validText(data.password) ? data.password : ''
+  data.password2 = validText(data.password2) ? data.password2 : '' //this is like confirm password
+
+  if (!Validator.isLength(data.handle, { min: 2, max: 30 })) {
+    errors.handle = 'Handle must be between 2 and 30 characters.'
+  }
+
+  if (Validator.isEmpty(data.handle)) {
+    errors.handle = 'Handle field is required.'
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required.'
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid'
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'Password is required'
+  }
+
+  if (!Validator.isLength(data.password, { min: 2, max: 30 })) {
+    errors.handle = 'Handle must be between 2 and 30 characters.'
+  }
+
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = 'Passwords must match'
+  }
+
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0,
+  }
+}
