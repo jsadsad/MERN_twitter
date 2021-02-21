5d039b72fb5e3199f3553274893ae754829afb0d
JCS <joshsad2@gmail.com>
2021-02-20 20:37:02 -0800

    user reg working

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const db = require('./config/keys').mongoURI
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser') //we can parse the JSON we send to our frontend
+const bodyParser = require('body-parser')
+// we can parse the JSON we send to our frontend
+// body parser tells our app what source of requests it should respond to
 
 const users = require('./routes/api/users')
 const tweets = require('./routes/api/tweets')
@@ -13,8 +15,16 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch((err) => console.log(err))
-
 mongoose.set('useNewUrlParser', true)
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+)
+app.use(bodyParser.json())
+//20-25 tells our app tghat we want it to respond to JSON requests.
+// urlencoded allows app to take requests from postman
 
 app.get('/', (req, res) => {
   const user = new User({
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/User')
 
 router.get('/test', (req, res) => res.json({ msg: 'This is the Users Route' }))
 module.exports = router
+
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      //if user already exists in db
+      return res
+        .status(400)
+        .json({ email: 'User already exists with that email.' })
+    } else {
+      const newUser = new User({
+        handle: req.body.handle,
+        email: req.body.email,
+        password: req.body.password,
+      })
+      newUser
+        .save()
+        .then((user) => res.send(user))
+        .catch((err) => res.send(err))
+    }
+  })
+})
